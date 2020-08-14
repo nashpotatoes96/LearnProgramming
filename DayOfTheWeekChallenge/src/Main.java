a34ea2903e11126870154c7203a625b1820273ac
naish <naish.oloughlin@pharmapodhq.com>
2020-08-14 14:47:20 +0100

    Added switch case challenge

---
+++ DayOfTheWeekChallenge/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        DayOfTheWeekChallenge.printDayOfTheWeek(0);
+        DayOfTheWeekChallenge.printDayOfTheWeek(1);
+        DayOfTheWeekChallenge.printDayOfTheWeek(2);
+        DayOfTheWeekChallenge.printDayOfTheWeek(3);
+        DayOfTheWeekChallenge.printDayOfTheWeek(4);
+        DayOfTheWeekChallenge.printDayOfTheWeek(5);
+        DayOfTheWeekChallenge.printDayOfTheWeek(6);
+
+        DayOfTheWeekChallenge.printDayOfTheWeek(7);
+        DayOfTheWeekChallenge.printDayOfTheWeek(-1);
+
+        DayOfTheWeekChallenge.printDayOfTheWeekIf(0);
+        DayOfTheWeekChallenge.printDayOfTheWeekIf(1);
+        DayOfTheWeekChallenge.printDayOfTheWeekIf(2);
+        DayOfTheWeekChallenge.printDayOfTheWeekIf(3);
+        DayOfTheWeekChallenge.printDayOfTheWeekIf(4);
+        DayOfTheWeekChallenge.printDayOfTheWeekIf(5);
+        DayOfTheWeekChallenge.printDayOfTheWeekIf(6);
+
+        DayOfTheWeekChallenge.printDayOfTheWeekIf(7);
+        DayOfTheWeekChallenge.printDayOfTheWeekIf(-1);
+    }
+}
