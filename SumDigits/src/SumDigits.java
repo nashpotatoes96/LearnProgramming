51220a94a8bfb62f4f1bc99b0eddcfccb383c4e1
naish <naish.oloughlin@pharmapodhq.com>
2020-08-21 17:25:38 +0100

    Added some while loop challenges

--- SumDigits/src/SumDigits.java
+++ SumDigits/src/SumDigits.java
@@ -11,4 +11,37 @@ public class SumDigits {
         }
         return sum;
     }
+
+    public static int sumFirstAndLastDigit (int number) {
+
+        if (number < 0)
+            return -1;
+
+        int last = number % 10;
+        int first = 0;
+
+        while (number > 0) {
+            if (number < 10) {
+                first = number;
+            }
+            number /= 10;
+        }
+
+        return last + first;
+    }
+
+    public static int getEvenDigitSum (int number) {
+        if (number < 0)
+            return -1;
+
+        int sum = 0;
+        while (number > 0) {
+            int remainder = number % 10;
+            if (remainder % 2 == 0) {
+                sum += remainder;
+            }
+            number /= 10;
+        }
+        return sum;
+    }
 }
