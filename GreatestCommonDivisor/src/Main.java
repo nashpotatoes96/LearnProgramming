c4da3cdaf0df65bcb1df348195259e21148dc165
Naish O'Loughlin <naisholoughlin@MacBook-Pro.local>
2020-08-30 22:36:25 +0100

    Added Highest Common Divisor challenge solution

---
+++ GreatestCommonDivisor/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("3 = " + GreatestCommonDivisor.getGreatestCommonDivisor(15, 18));
+        System.out.println("2 = " + GreatestCommonDivisor.getGreatestCommonDivisor(14, 16));
+        System.out.println("15 = " + GreatestCommonDivisor.getGreatestCommonDivisor(15, 60));
+        System.out.println("-1 = " + GreatestCommonDivisor.getGreatestCommonDivisor(9, 30));
+        System.out.println("1 = " + GreatestCommonDivisor.getGreatestCommonDivisor(11, 27));
+
+    }
+}
