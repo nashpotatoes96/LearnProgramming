822b4bb0dcc7f6a3ceefce5647222b71d3fd487c
Naish O'Loughlin <naisholoughlin@MacBook-Pro.local>
2020-08-30 22:51:09 +0100

    Added All Factors challenge solution

---
+++ AllFactors/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        FactorPrinter.printFactors(6);
+        FactorPrinter.printFactors(24);
+        FactorPrinter.printFactors(878);
+        FactorPrinter.printFactors(23);
+        FactorPrinter.printFactors(-3);
+    }
+}
