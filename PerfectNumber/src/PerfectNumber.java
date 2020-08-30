f1026c4af6ab58a5806c6867ef76efe04500a567
Naish O'Loughlin <naisholoughlin@MacBook-Pro.local>
2020-08-30 23:14:30 +0100

    Added Perfect Number challenge solution

---
+++ PerfectNumber/src/PerfectNumber.java
@@ -0,0 +1,19 @@
+public class PerfectNumber {
+
+    public static boolean isPerfectNumber (int number) {
+
+        if (number < 1) {
+            return false;
+        }
+
+        int sumFactors = 0;
+        for (int i = number; i >= 1; i--) {
+
+            if ((number % i == 0) && i != number) {
+                sumFactors += i;
+            }
+        }
+
+        return sumFactors == number;
+    }
+}
