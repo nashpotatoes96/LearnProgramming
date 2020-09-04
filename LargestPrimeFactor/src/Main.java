6605b7f00e7a363cf25c6a9f770d2627adb8cc4b
Naish O'Loughlin <naisholoughlin@MacBook-Pro.local>
2020-09-04 15:28:51 +0100

    Added LargestPrimeFactor challenge

---
+++ LargestPrimeFactor/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(LargestPrime.getLargestPrime(21));
+        System.out.println(LargestPrime.getLargestPrime(7));
+        System.out.println(LargestPrime.getLargestPrime(8));
+        System.out.println(LargestPrime.getLargestPrime(217));
+
+    }
+}
