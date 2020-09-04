7a4d9d1c327600b86727c9e7dc0c2bd491c7a445
Naish O'Loughlin <naisholoughlin@MacBook-Pro.local>
2020-09-04 14:37:26 +0100

    Added FlourPacker challenge

---
+++ FlourPackProblem/src/FlourPacker.java
@@ -0,0 +1,23 @@
+public class FlourPacker {
+
+    static int bigWeight = 5;
+
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+
+        if (bigCount < 0 || smallCount < 0 || goal < 0) {
+            return false;
+        }
+
+        for (int i = 0; i <= bigCount; i++) {
+            if (goal >= i * bigWeight) {
+                for (int j = 0; j <= smallCount; j++) {
+                    if ((i * bigWeight + j) == goal) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+}
